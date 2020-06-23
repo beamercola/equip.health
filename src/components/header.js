4b7858ee39b8af6cd9937d632df12557394f7001
Beamer <beamercola@gmail.com>
2020-06-23 00:24:08 -0700

    Navigation

--- src/components/header.js
+++ src/components/header.js
@@ -9,72 +9,66 @@ const Header = () => {
   const sticky = scrollY > 50
 
   return (
-    <header
-      className={`my-4 py-2 sticky top-0 z-50 bg-yellow-100 border-b transition-all duration-300 ${
-        sticky ? "border-blue-900" : "border-transparent"
-      }`}
-    >
-      <nav className="py-1">
-        <div className="container flex items-center justify-between">
-          <Link className="w-1/2 lg:w-1/4" to="/">
-            <Wordmark
-              className={`max-w-none fill-navy transition-all duration-100 ${
-                sticky ? "h-6" : "h-8"
-              }`}
-            />
-          </Link>
-
-          {!open && (
-            <>
-              <Links className="hidden lg:flex" />
-              <Actions className="hidden lg:flex" />
-            </>
-          )}
-
-          <button
-            type="button"
-            className="inline-flex lg:hidden items-center justify-center p-2 rounded-md text-blue-900 focus:outline-none focus:bg-yellow-600 focus:text-yellow-100 transition duration-150 ease-in-out"
-            onClick={() => setOpen(!open)}
-          >
-            <Hamburger />
-          </button>
-        </div>
-        <div className="relative">
-          {open && (
-            <div className="inset-x-0 absolute bg-yellow-100 z-50 container border-b border-blue-900 pb-4">
-              <Links />
-              <Actions />
+    <>
+      <header
+        className={`py-6 sticky top-0 z-50 bg-yellow-100 border-b transition-all duration-300 ${
+          sticky || open ? "border-blue-900" : "border-transparent"
+        }`}
+      >
+        <nav className="py-1">
+          <div className="container flex flex-col lg:flex-row lg:items-center justify-between">
+            <div className="flex w-full justify-between items-center">
+              <Link className="lg:w-1/4" to="/">
+                <Wordmark
+                  className={`max-w-none fill-navy transition-all duration-100 ${
+                    sticky ? "h-6" : "h-8"
+                  }`}
+                />
+              </Link>
+              <button
+                type="button"
+                className="inline-flex lg:hidden items-center justify-center p-2 rounded-md text-blue-900 focus:outline-none focus:bg-yellow-600 focus:text-yellow-100 transition duration-150 ease-in-out"
+                onClick={() => setOpen(!open)}
+              >
+                <Hamburger />
+              </button>
             </div>
-          )}
-        </div>
-      </nav>
-    </header>
+
+            <div
+              className={`lg:w-2/4 lg:justify-center flex flex-col tracking-wider text-lg py-8 lg:py-0 lg:flex-row transition-all ${
+                !open && "hidden"
+              }`}
+            >
+              <NavItem to="/how-it-works">How It Works</NavItem>
+              <NavItem to="/team">Team</NavItem>
+              <NavItem to="/contact">Contact</NavItem>
+            </div>
+
+            <div
+              className={`flex lg:w-1/4 lg:justify-end items-center lg:mt-0 ${
+                !open && "hidden"
+              }`}
+            >
+              <Link
+                className="py-2 text-sm whitespace-no-wrap order-2 lg:order-1"
+                to="/login"
+              >
+                Log In
+              </Link>
+              <SignUpButton className="lg:ml-4 mr-4 lg:mr-0 order-1" />
+            </div>
+          </div>
+        </nav>
+      </header>
+      <div
+        className={`bg-black cursor-pointer inset-0 fixed z-30 opacity-25 transform duration-300 transition-all ${
+          open ? "bg-black" : "bg-transparent hidden"
+        }`}
+        onClick={() => setOpen(false)}
+      />
+    </>
   )
 }
-
-const Links = ({ className }) => (
-  <div
-    className={`w-2/4 justify-center flex flex-col tracking-wider text-lg py-8 lg:py-0 lg:flex-row ${className}`}
-  >
-    <NavItem to="/how-it-works">How It Works</NavItem>
-    <NavItem to="/team">Team</NavItem>
-    <NavItem to="/contact">Contact</NavItem>
-  </div>
-)
-
-const Actions = ({ className }) => (
-  <div
-    className={`flex lg:w-1/2 lg:w-1/4 lg:justify-end items-center lg:mt-0 ${className}`}
-  >
-    <Link
-      className="py-2 text-sm whitespace-no-wrap order-2 lg:order-1"
-      to="/login"
-    >
-      Log In
-    </Link>
-    <SignUpButton className="lg:ml-4 mr-4 lg:mr-0 order-1" />
-  </div>
-)
 
 const NavItem = ({ children, to }) => {
   const getLinkProps = ({ isPartiallyCurrent }) => {
