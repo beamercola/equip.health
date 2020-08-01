f605c8c62febf142cb21728427bedf05cebca801
Beamer <beamercola@gmail.com>
2020-07-31 22:56:51 -0700

    Recruit decoupling

--- src/components/forms/Recruit.js
+++ src/components/forms/Recruit.js
@@ -1,56 +1,38 @@
-import React, { useState } from "react"
+import React from "react"
 import { states } from "../forms"
 import Input from "./Input"
 import Select from "./Select"
 
+const positions = [
+  "Therapist",
+  "Dietitian",
+  "Peer Mentor",
+  "Family Mentor",
+  "MD",
+  "Other",
+]
+
 const Recruit = ({ callToAction }) => {
-  const positions = [
-    "Therapist",
-    "Dietitian",
-    "Peer Mentor",
-    "Family Mentor",
-    "MD",
-    "Other",
-  ]
-  const [params, setParams] = useState({
-    name: "",
-    email: "",
-    position: positions[0],
-    state: states[0].value,
-    license: "",
-    message: "",
-  })
-
-  const handleChange = e => {
-    setParams({ ...params, [e.target.name]: e.target.value })
-  }
-
-  const handleSubmit = e => {
-    e.preventDefault()
-    e.target.submit()
-  }
-
   return (
     <form
       name="Recruit"
       method="post"
       data-netlify="true"
       data-netlify-honeypot="bot-field"
-      onSubmit={handleSubmit}
     >
       <input type="hidden" name="bot-field" />
       <input type="hidden" name="form-name" value="Recruit" />
       <div className="my-6 w-full">
         <label htmlFor="name">Name</label>
-        <Input type="text" name="name" required onChange={handleChange} />
+        <Input type="text" name="name" required />
       </div>
       <div className="my-6 w-full">
         <label htmlFor="name">Email</label>
-        <Input type="text" name="email" required onChange={handleChange} />
+        <Input type="text" name="email" required />
       </div>
       <div className="my-6 w-full">
         <label htmlFor="state">State</label>
-        <Select name="state" required onChange={handleChange}>
+        <Select name="state" required>
           {states.map(state => (
             <option key={state.value}>{state.value}</option>
           ))}
@@ -58,7 +40,7 @@ const Recruit = ({ callToAction }) => {
       </div>
       <div className="my-6 w-full">
         <label htmlFor="position">Desired Position</label>
-        <Select name="position" required onChange={handleChange}>
+        <Select name="position" required>
           {positions.map(position => (
             <option key={position}>{position}</option>
           ))}
@@ -66,14 +48,12 @@ const Recruit = ({ callToAction }) => {
       </div>
       <div className="my-6 w-full">
         <label htmlFor="license">License (if applicable)</label>
-        <Input type="text" name="license" onChange={handleChange} />
+        <Input type="text" name="license" />
       </div>
       <div className="my-6 w-full">
         <label htmlFor="message">Optional Message</label>
         <textarea
           className="w-full p-4 rounded-lg text-navy-300"
-          value={params.message}
-          onChange={handleChange}
           name="message"
         />
       </div>
