2390288533e3efac1e2e1edcbe63704855fdc936
Beamer <beamercola@gmail.com>
2020-10-07 11:39:13 -0700

    Update press.js

--- src/components/blocks/press.js
+++ src/components/blocks/press.js
@@ -5,21 +5,22 @@ const Press = ({ press: { title, companies } }) => {
   const [company, setCompany] = useState(companies[0])
 
   return (
-    <div className="px-16 py-16">
+    <div className="px-8 md:px-16 py-16">
       <h2 className="text-5xl text-center text-navy-300 mb-4">{title}</h2>
       <blockquote
-        className="py-12 md:px-24 lg:px-48 text-4xl italic text-center"
+        className="py-12 md:px-24 lg:px-48 text-xl md:text-3xl italic text-center"
         dangerouslySetInnerHTML={{ __html: company.quoteHtml }}
       />
       <div className="flex flex-wrap -mx-8 items-center justify-center">
-        {companies.map(company => (
+        {companies.map((company, i) => (
           <div
-            className="px-8 w-1/3 md:w-1/5 cursor-pointer"
+            className="px-8 w-1/2 md:w-1/5 cursor-pointer text-center"
             onClick={() => setCompany(company)}
             onMouseEnter={() => setCompany(company)}
+            key={i}
           >
             <img
-              className="h-16 object-contain"
+              className="h-16 object-contain mx-auto"
               src={getImageUrl(company.logo.path)}
               alt=""
             />
