51bb47099c9fec45e8dbd05366203b0253b35e58
Beamer <beamer@hey.com>
2021-01-26 18:51:54 -0800

    Update Header.js

--- src/components/Articles/Header.js
+++ src/components/Articles/Header.js
@@ -1,18 +1,38 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 import { Hero } from "../Blocks"
 import { Nav } from "../Articles"
 import Link from "../Link"
 
-const Header = () => (
-  <Hero>
-    <div className="text-center py-12">
-      <h1 className="text-4xl font-light mb-1">
-        <Link to="/articles">Reconvene</Link>
-      </h1>
-      <p>A blog about eating disorders and recovery in a modern world</p>
-    </div>
-    <Nav />
-  </Hero>
-)
+const Header = () => {
+  const {
+    takeshape: {
+      getBlog: { hero },
+    },
+  } = useStaticQuery(graphql`
+    query BlogHeader {
+      takeshape {
+        getBlog {
+          hero {
+            title
+            description
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Hero>
+      <div className="text-center py-16 md:py-24">
+        <h1 className="text-4xl font-light mb-1">
+          <Link to="/articles">{hero.title}</Link>
+        </h1>
+        <p>{hero.description}</p>
+      </div>
+      <Nav />
+    </Hero>
+  )
+}
 
 export default Header
